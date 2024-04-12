73de285b287a599bc5a986bc8c34898612a0eaef
Matthew Brady <matthewn@leia1.cm.cluster>
2024-04-12 09:33:52 -0500

    added t5 eval

---
+++ ft_model_t5_eval.py
@@ -0,0 +1,100 @@
+# Load imports
+import torch
+import random
+import numpy as np
+import pandas as pd
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import Dataset
+from sentence_transformers import SentenceTransformer
+
+pd.set_option('display.max_colwidth', None)
+ 
+# Implement T5 comparison
+model = SentenceTransformer('sentence-t5-base')
+
+def sharpened_cosine_similarity(vec1, vec2, exponent=3):
+    cosine_similarity = torch.nn.functional.cosine_similarity(vec1, vec2, dim=0)
+    return cosine_similarity ** exponent
+
+# Compare the similarity of the phrases
+def compare_phrases(test_phrase, predicted_phrase):
+    print(f"actual instruction: {test_phrase}")
+    print(f"predicted instruction: {predicted_phrase}")
+
+    test_embedding = model.encode(test_phrase, convert_to_tensor=True, show_progress_bar=False)
+    
+    compare_embedding = model.encode(predicted_phrase, convert_to_tensor=True, show_progress_bar=False)
+    score = sharpened_cosine_similarity(test_embedding, compare_embedding).item()
+    
+    print(f"Similarity score: {score}\n")
+
+    return(test_phrase, predicted_phrase, score)
+
+# Define the function to generate completions
+def get_completion_merged(input_text: str, output_text: str, model, tokenizer) -> str:
+    prompt_template = """
+    <s>
+    [INST]
+    In few words, how was the input modified to obtain the output?
+    ##Input: {input}
+    ##Output: {output}
+    [/INST]
+    </s>
+    """
+    prompt = prompt_template.format(input=input_text, output=output_text)
+    encodeds = tokenizer(prompt, return_tensors="pt", add_special_tokens=True)
+    model_inputs = encodeds.to(device)
+    generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+    #decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    # Get prompt length to remove the prepended prompt from the output
+    prompt_length = model_inputs.shape[1]
+    print(prompt_length)
+    decoded = tokenizer.batch_decode(generated_ids[0][prompt_length:], skip_special_tokens=True)
+    return decoded[0]
+
+
+# Model setup
+#model_id = "/home/matthewn/kaggle/llm_prompt_recovery/merged_mistral_prompt_recovery_fine_tuned_base_model" # base model
+model_id="/home/matthewn/kaggle/llm_prompt_recovery/merged_mistral_prompt_recovery_model_2024-04-11_12_fine_tuned_instruction_model" # instruct model
+tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side="left")
+tokenizer.pad_token = '[PAD]'
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+merged_model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2"
+)
+merged_model.eval()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+# Read the test data from JSON
+test_data_path = '/home/matthewn/kaggle/llm_prompt_recovery/training_test_data/test_data.json'
+# Correct the reading method to handle line-delimited JSON
+test_data = pd.read_json(test_data_path, lines=True)
+
+test_data['text'] = test_data['text'].apply(lambda x: eval(x) if isinstance(x, str) else x)
+
+# Normalize the 'text' column to create a DataFrame
+test_data = pd.json_normalize(test_data['text'])
+
+
+# Evaluate each entry in the test data and write results to a file
+model_name = model_id.split("/")[-1]
+output_filename = f'model_eval_{model_name}.txt'
+with open(output_filename, 'w') as file:
+    for index, row in test_data.iterrows():
+        try:
+            generated_instruction = get_completion_merged(row['input'], row['output'], merged_model, tokenizer)
+            comparison_result = compare_phrases(row['instruction'], generated_instruction)
+            result_line = f"Actual Instruction: {row['instruction']}\nGenerated Instruction: {generated_instruction}\nComparison: {comparison_result}\n---\n"
+            file.write(result_line)
+            file.write("test")
+        except Exception as e:
+            error_msg = f"Error processing row {index}: {e}\n"
+            file.write(error_msg)
+
+print(f"Results have been written to {output_filename}.")
--- recover_prompt_mistral7b.py
+++ recover_prompt_mistral7b.py
@@ -11,9 +11,12 @@ import os
 from trl import SFTTrainer
 
 # Getting model and tokenizer
-model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+#model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+# NOTE: Trying base model
+#model_id = "mistralai/Mistral-7B-v0.1" # base model
+model_id = "mistralai/Mistral-7B-Instruct-v0.2" # instruct model
 tokenizer = AutoTokenizer.from_pretrained(model_id, add_eos_token=True)
-tokenizer.padding_side = 'right'
+tokenizer.padding_side = 'left'
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
 bnb_config = BitsAndBytesConfig(
@@ -27,7 +30,7 @@ bnb_config = BitsAndBytesConfig(
 path = "/home/matthewn/kaggle/llm_prompt_recovery/"
 
 def is_output_valid(output):
-    invalid_sections = ["cannot answer this", "not able to provide an excerpt", "offensive", "discriminate"]
+    invalid_sections = ["cannot answer", "not able to provide", "am not able to", "offensive", "discriminate", "inappropriate", "harmful", "violence", "abuse", "hate", "harassment", "insensitive", "sexual"]
     for section in invalid_sections:
         if section in output.lower():
             return False
@@ -48,7 +51,7 @@ def remove_first_portion(output):
 
 dataset = []
 for json_file in os.listdir(path):
-    if json_file.endswith(".json"):
+    if json_file.endswith("training_data*.json"):
         full_path = os.path.join(path, json_file)
 
         with open(full_path, 'r') as file:
@@ -72,7 +75,14 @@ for json_file in os.listdir(path):
                         instruction = item.get('prompt', '')  # Using .get to avoid KeyError
                         input_text = item.get('original_text', '')  # Using .get to avoid KeyError
                         # Compose the 'text' field
-                        text = f"<s>[INST] {instruction} Here is the input: {input_text} [/INST] {output}</s>"
+                        # text = f"<s>[INST] {instruction} Here is the input: {input_text} [/INST] {output}</s>"
+                        # reordered_item = {
+                        #     'instruction': instruction,
+                        #     'input': input_text,
+                        #     'output': output,
+                        #     'text': text  # Add the composed 'text' field
+                        # }
+                        text = f"<s>[INST] In few words, how was the input modified to obtain the output? ##Input: {input_text} ##Output: {output} [/INST] {instruction}</s>"
                         reordered_item = {
                             'instruction': instruction,
                             'input': input_text,
@@ -88,7 +98,8 @@ for json_file in os.listdir(path):
 
 # You need to convert your list to a Dataset object to use 'train_test_split'
 dataset = Dataset.from_dict({'text': dataset})
-dataset = dataset.train_test_split(test_size=0.2)
+# Shuffle is false while comparing fine tuning of base vs instruct model
+dataset = dataset.train_test_split(test_size=0.2, shuffle=False)
 train_data = dataset['train']
 test_data = dataset['test']
 
@@ -111,9 +122,12 @@ for item in test_data['text']:
 test_data_df = pd.DataFrame(test_data_list, columns=['text'])
 test_data_df = Dataset.from_pandas(test_data_df)
 
-# TODO: Testing this
-train_data_df = train_data_df.map(lambda samples: tokenizer(samples["text"], truncation=True, padding='max_length', max_length=512), batched=True)
-test_data_df = test_data_df.map(lambda samples: tokenizer(samples["text"], truncation=True, padding='max_length', max_length=512), batched=True)
+train_data_df = train_data_df.map(lambda samples: tokenizer(samples["text"], truncation=True, padding='max_length', max_length=4000), batched=True)
+test_data_df = test_data_df.map(lambda samples: tokenizer(samples["text"], truncation=True, padding='max_length', max_length=4000), batched=True)
+
+# Save the DataFrame to a JSON file, using the 'records' format to make each row a separate JSON object
+test_data_df.to_json('/home/matthewn/kaggle/llm_prompt_recovery/test_data.json', orient='records', lines=True)
+train_data_df.to_json('/home/matthewn/kaggle/llm_prompt_recovery/train_data.json', orient='records', lines=True)
 
 
 # While using SFT (Supervised Fine-tuning Trainer) for fine-tuning, we will be only passing in the “text” column of the dataset for fine-tuning.
@@ -128,7 +142,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #    #attn_implementation="flash_attention_2"
 #)
 
-model = AutoModelForCausalLM.from_pretrained('mistralai/Mistral-7B-Instruct-v0.2', quantization_config=bnb_config, torch_dtype=torch.float16, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, torch_dtype=torch.float16, device_map="auto")
 
 
 
@@ -169,6 +183,7 @@ print(f"Trainable: {trainable} | total: {total} | Percentage: {trainable/total*1
 
 import transformers
 
+# NOTE: Trying 1000 steps instead of 100
 trainer = SFTTrainer(
     model=model,
     train_dataset=train_data_df,
@@ -231,4 +246,4 @@ model_name = f"merged_mistral_prompt_recovery_model_{now}"
 merged_model.save_pretrained(model_name, safe_serialization=True)
 tokenizer.save_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
+tokenizer.padding_side = "left"
