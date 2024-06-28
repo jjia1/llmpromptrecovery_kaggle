ada6ff2f0df492c8faebaa70ca329e4a83161fed
jjia <jdjia93@gmail.com>
2024-06-28 17:29:14 -0500

    removed old and mpi_run_wrapper.sh

--- mpi_run_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module load openmpi/gcc/64/4.1.5
-module load cuda12.2/toolkit/12.2.2
-export PYTHONUNBUFFERED=1
-mpirun -np 1 --output-filename ${1%.*}/output --prefix /cm/shared/apps/openmpi4/gcc/4.1.5 --host sabercore-a100-002 /home/johnathanj/.conda/envs/llama2/bin/python  $1
-
